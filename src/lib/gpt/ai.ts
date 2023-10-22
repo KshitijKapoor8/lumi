8789ef64d3941ad36763baed0f12d7a827df055b
KshitijKapoor8 <kshitijkapoor8@gmail.com>
2023-10-21 21:58:12 -0500

    bro

---
+++ src/lib/gpt/ai.ts
@@ -0,0 +1,9 @@
+import OpenAI from 'openai'
+import { OPENAI_KEY } from '$env/static/private'
+
+
+const ai = new OpenAI({
+    apiKey: OPENAI_KEY,
+})
+
+export default ai;
--- src/lib/server/server.ts
+++ src/lib/server/server.ts
@@ -3,6 +3,7 @@ import { defineSources } from "./models/Sources.model";
 import { defineCategories } from "./models/Categories.model";
 import { defineCounts } from "./models/Counts.model";
 import { POSTGRES_DB, POSTGRES_PASSWORD, POSTGRES_USERNAME, POSTGRES_HOST } from "$env/static/private";
+import fs from "fs"
 
 const sequelize = 
   new Sequelize(`postgres://${POSTGRES_USERNAME}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:5432/${POSTGRES_DB}`)
@@ -18,5 +19,14 @@ Sources.belongsToMany(Categories, { through: Counts })
 
 await sequelize.sync({ force: true })
 console.log("All models were synchronized successfully.")
+
+
+// const allContents = fs.readFileSync('../../../cat.txt', 'utf-8');
+// allContents.split(/\r?\n/).forEach((line) => {
+//   console.log(line)
+//     // Categories.create({ name: line })
+//     // Sources.create({ name: source })
+//     // Counts.create({ count: count })
+// });
 
 export { sequelize, Categories, Sources, Counts }
