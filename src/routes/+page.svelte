42a79e81be88b50de9f882a598666df284c1fd9a
tanya-jagan <tanyajagan19@gmail.com>
2023-10-21 22:43:19 -0500

    asf

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,13 +8,13 @@
   }
 </script>
 
-<div class="flex flex-col bg-[#16262E] h-screen w-screen justify-center items-center ">
-  <h1 class="text-4xl font-bold font-mono text-slate-400">
+<div class="flex flex-col h-screen w-screen justify-center items-center bg-wave bg-no-repeat bg-cover bg-center">
+  <h1 class="text-7xl font-bold font-mono text-slate-400">
     Lumi
   </h1>
   <div class = "pt-5 min-w-[25%]">
     <form class>
-      <label for="default-search" class="mb-2 text-sm font-large text-gray-900 sr-only dark:text-white">Search</label>
+      <label for="default-search" class="mb-2 text-2x1 font-large text-gray-900 sr-only dark:text-white">Search</label>
     <div class="relative">
         <div class="absolute pt-4 left-0 items-center pl-3">
           <svg class="h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
@@ -25,7 +25,7 @@
           <button type="submit"> </button>
     </form>
   </div>
-  <img class="h-14 w-14 object-cover absolute bottom-0 left-0" src="/copymouse.png" alt="temporary logo">
+  <img class="h-[25%] object-cover absolute bottom-5 left-5" src="/tempLumiLogo2.png" alt="temporary logo">
 </div>
 
 <style lang="postcss">
